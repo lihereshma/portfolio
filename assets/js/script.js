0aea3927b155e6b3a688f2f07f51ff5b992a4a05
lihereshma <reshmalihe00068@gmail.com>
2021-07-27 12:42:37 +0530

    folder updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,81 +60,17 @@ function scrollActive(){
 }
 
 // ---------------------- animation ------------------------------------------------------------
-// const sr = ScrollReveal({
-//   origin: 'top',
-//   distance: '80px',
-//   duration: 2000,
-//   reset: true
-// })
-// sr.reveal('.home__title', {})
-// sr.reveal('.home__scroll', { delay: 200 })
-// sr.reveal('.home__img', { origin: 'right', delay: 400 })
-// sr.reveal('.about__img', { delay: 500 })
-// sr.reveal('.about__subtitle', { delay: 300 })
-// sr.reveal('.about__profession', { delay: 400 })
-// sr.reveal('.about__button', { delay: 500 })
-// sr.reveal('.skills__box', { distance: '20px', delay: 50, interval: 100 })
-// sr.reveal('.skills__img', {delay: 400})
-// sr.reveal('.portfolio__img', { interval: 200 })
-// sr.reveal('.contact__info', { delay: 400 })
-// sr.reveal('.contact__form', { delay: 400 })
-
-// ----------------------- form validation -------------------------------------------------------
-const name = document.getElementById('name').parentNode;
-const email = document.getElementById('email').parentNode;
-const message = document.getElementById('message');
-
-const RegexExpression = {
-  username_regex: /^[a-zA-Z ]*$/,
-  email_regex: /^([0-9a-zA-Z\_\.\-]+)@([0-9a-zA-Z\_\.\-]+)\.([a-zA-Z]+)$/
-}
-
-let inputArray = Array.from(document.querySelectorAll('.contact__inputs input'));
-
-inputArray.forEach(element => {
-  element.addEventListener('keyup', () => {
-    validateInput(element);
-  });
-});
-
-let validateInput = (input) => {
-  var requiredRegex = input.getAttribute('data-regex');
-  var parent = input.parentNode;
-  
-  if(input.value == "") {
-    parent.classList = "form-group";
-  }
-  else if(RegexExpression[requiredRegex].test(input.value)) {
-    parent.classList = "form-group success"
-  } else {
-    parent.classList = "form-group error"
-  }
-}
-
-message.addEventListener('keyup', () => {
-  const lng = message.value.length;
-  document.getElementById('charNum').innerHTML = `${lng} out of 200 characters`;
-  if(lng < 1 || lng > 200) {
-    message.parentNode.classList = "form-group error"
-  } else {
-    message.parentNode.classList = "form-group success"
-  }
+const sr = ScrollReveal({
+  origin: 'bottom',
+  distance: '80px',
+  duration: 2000,
+  // reset: true
 })
-
-
-
-var button = document.querySelector('.contact__button');
-
-button.addEventListener('click', notify);
-function notify() {
-  if(name.classList.contains('success') && email.classList.contains('success')
-      && message.parentNode.classList.contains('success')) {
-    // alert('Your submission has been received. We will be in touch shortly.');
-    window.location.reload();
-  } else {
-    alert('Error: Fill up all the fields correctly.');
-  }
-}
+sr.reveal('.about__img', { origin: 'left', delay: 300 })
+sr.reveal('.about__content', { origin: 'right', delay: 300 })
+sr.reveal('.skills__box li', { delay: 100, interval: 100 })
+sr.reveal('.contact__info', { origin: 'left', delay: 400 })
+sr.reveal('.contact__image', { origin: 'right', delay: 400 })
 
 $(document).ready(function(){
   $('a#filter-a').click(function(){
